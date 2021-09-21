3fa2150f73663f86440c79b2b2270df73779a8b5
Jacob Reiter <jreiter@klhengrs.com>
2021-09-21 18:03:09 -0400

    added tests

--- UnitTesting.GettingStarted.Tests/CalculatorTests.cs
+++ UnitTesting.GettingStarted.Tests/CalculatorTests.cs
@@ -13,12 +13,35 @@ namespace UnitTesting.GettingStarted.Tests
         [TestCase(1,2)]
         [TestCase(2, 3)]
         [TestCase(3, 5)]
-        [TestCase(1000, 1)]
+        [TestCase(1, 1000)]
         public void Add_Always_ReturnsExpectedResult(int lhs, int rhs)
         {
             var systemUnderTest = new Calculator();
             var expected = lhs + rhs;
             Assert.That(systemUnderTest.Add(lhs, rhs), Is.EqualTo(expected));
+        }
+
+
+        [TestCase(1, 2)]
+        [TestCase(2, 3)]
+        [TestCase(3, 5)]
+        [TestCase(1, 1000)]
+        public void SubtractLargeFromSmall_LargeFirst_ReturnsExpectedResult(int smallNum, int largeNum)
+        {
+            var systemUnderTest = new Calculator();
+            var expected = largeNum - smallNum;
+            Assert.That(systemUnderTest.SubtractLargeFromSmall(largeNum, smallNum), Is.EqualTo(expected));
+        }
+
+        [TestCase(1, 2)]
+        [TestCase(2, 3)]
+        [TestCase(3, 5)]
+        [TestCase(1, 1000)]
+        public void SubtractLargeFromSmall_SmallOrEqualFirst_ReturnsExpectedResult(int smallNum, int largeNum)
+        {
+            var systemUnderTest = new Calculator();
+            var expected = largeNum - smallNum;
+            Assert.That(systemUnderTest.SubtractLargeFromSmall(smallNum, largeNum), Is.EqualTo(expected));
         }
     }
 }
